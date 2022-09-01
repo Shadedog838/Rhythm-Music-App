f9d68c33fc3f17525ff1c703ffb14d409c628918
Daniel Chapin <dchapes123@gmail.com>
2022-09-01 09:09:55 -0400

    Add 404 page and logging

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+const port = 3000;
 
 // Static hosting from the static folder
 app.use(express.static(`${__dirname}/static/`));
 
+// 404 page
+app.get('*', (req, res) => {
+  res.send("You've entered an invalid URL (404)");
+});
+
 // Start listening on the specified port
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Program listening on port ${port}`);
+});
