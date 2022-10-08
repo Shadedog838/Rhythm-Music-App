9808453278e142e99da921434b2b09b2cc41ea7d
Daniel Chapin <dchapes123@gmail.com>
2022-10-08 15:25:16 -0400

    Endpoint abstractions

---
+++ endpoints.js
@@ -0,0 +1,32 @@
+// Endpoint templates are stored here and POST endpoints are automatically
+// generated on program launch.
+//
+// Format:
+// const endpoints = [
+//     {
+//         path: '/sql',
+//         query: 'select ${attribs} from ${table}',
+//         params: {
+//             attribs: '*',
+//             table: undefined
+//         }
+//     }
+// ]
+// path is the path of the endpoint
+// query is the query string the request makes
+// params is an object that contains all POST parameters that will be substituted in to the query.
+//        The value in this object is the default value the query should use if it's not specified in the post request.
+//        If the default value is undefined, that parameter is considered required. An error will be returned if that param is not supplied.
+
+const endpoints = [
+    {
+        path: '/sql',
+        query: 'select ${attribs} from ${table}',
+        params: {
+            attribs: '*',
+            table: undefined
+        }
+    },
+]
+
+module.exports = { endpoints };
--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const port = 3000;
 const db = require('./db.js');
 db.connect();
 
+// Generated endpoints:
+const { endpoints } = require('./endpoints.js');
+
+// JSON parsing for POST requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Static hosting from the static folder
 app.use(express.static(`${__dirname}/static/`));
 
@@ -21,6 +28,48 @@ app.use(express.static(`${__dirname}/static/`));
 app.get('*', (req, res) => {
   res.send("You've entered an invalid URL (404)");
 });
+
+// ================= //
+// === HTTP POST === //
+// ================= //
+
+// Template defined endpoints
+(() => {
+    for (let i in endpoints) {
+        const endpoint = endpoints[i];
+        const path = endpoint.path;
+        const query = endpoint.query;
+        const params = endpoint.params;
+
+        app.post(path, (req, res) => {
+            const user_params = req.body;
+            console.log(req);
+            console.log('params', user_params);
+            let subs = Object.assign({}, params);
+            subs = Object.assign(subs, user_params);
+            let realized_query = query.slice();
+            for (let key in subs) {
+                const replacement = subs[key];
+                const target = `\$\{${key}\}`;
+                if (!replacement) {
+                    res.status(409).send(`POST Endpoint ${path} requires parameter ${key}, but it was not specified.`);
+                    return;
+                }
+                realized_query = realized_query.replaceAll(target, replacement);
+            }
+
+            db.query(realized_query)
+            .then((data) => {
+                res.status(200).send(data.rows);
+            })
+            .catch((err) => {
+                console.log(`Error occured in POST ${path}`);
+                console.log(err);
+                res.status(500).send(err.toString());
+            });
+        });
+    }
+})();
 
 // Start listening on the specified port
 app.listen(port, () => {
