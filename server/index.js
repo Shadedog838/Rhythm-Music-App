a5a4e7dbffbe6d2d96a22ada648467eac16a2fe9
sam8031 <sam8031@g.rit.edu>
2022-10-13 09:00:07 -0400

    created song and user files

--- server/index.js
+++ server/index.js
@@ -4,72 +4,14 @@ const app = express();
 const cors = require("cors")
 const port = 5000;
 
+
+app.use("./user", require("./routes/user"));
+app.use("./song", require("./routes/song"));
+
 // middleware
 const pool = require('./db');
 app.use(express.json());
 
-// Generated endpoints:
-// const { endpoints } = require('./endpoints.js');
-
-// JSON parsing for POST requests
-// app.use(express.urlencoded({ extended: true }));
-
-
-// routes
-
-// ================ //
-// === HTTP GET === //
-// ================ //
-
-// app.get("/test", async (req, res) => {
-//     try {
-//         const allNames = await pool.query("SELECT * FROM test");
-//         res.json(allNames.rows);
-//     } catch (err) {
-//         console.log(err.message);
-//     }
-// });
-
-
-// ================= //
-// === HTTP POST === //
-// ================= //
-
-// Template defined endpoints
-// (() => {
-//     for (let i in endpoints) {
-//         const endpoint = endpoints[i];
-//         const path = endpoint.path;
-//         const query = endpoint.query;
-//         const params = endpoint.params;
-
-//         app.post(path, (req, res) => {
-//             const user_params = req.body;
-//             let subs = Object.assign({}, params);
-//             subs = Object.assign(subs, user_params);
-//             let realized_query = query.slice();
-//             for (let key in subs) {
-//                 const replacement = subs[key];
-//                 const target = `\$\{${key}\}`;
-//                 if (!replacement) {
-//                     res.status(409).send(`POST Endpoint ${path} requires parameter ${key}, but it was not specified.`);
-//                     return;
-//                 }
-//                 realized_query = realized_query.replaceAll(target, replacement);
-//             }
-
-//             db.query(realized_query)
-//             .then((data) => {
-//                 res.status(200).send(data.rows);
-//             })
-//             .catch((err) => {
-//                 console.log(`Error occured in POST ${path}`);
-//                 console.log(err);
-//                 res.status(500).send(err.toString());
-//             });
-//         });
-//     }
-// })();
 
 // Start listening on the specified port
 app.listen(port, () => {
---
+++ server/routes/user.js
@@ -0,0 +1,1 @@
+// Routes for all USER related operations //
