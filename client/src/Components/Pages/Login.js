743fd7e17ded13254c27b59866780ef81f2429f3
sam8031 <sam8031@g.rit.edu>
2022-10-18 22:09:20 -0400

    finished login page

--- client/src/Components/Pages/Login.js
+++ client/src/Components/Pages/Login.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import { Row, Col, Grid } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import { toast } from "react-toastify";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Fragment, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
+import { Link } from "react-router-dom";
+import "./css/Login.scss";
 
 export default function Login() {
   const [validated, setValidated] = useState(false);
@@ -16,38 +17,60 @@ export default function Login() {
       event.stopPropagation();
     }
 
-
     setValidated(true);
   };
 
-
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-     <Row className="mb-3">
-        {/* <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Username</Form.Label>
-          <InputGroup hasValidation>
-            <Form.Control
-              type="text"
-              placeholder="Username"
-              aria-describedby="inputGroupPrepend"
-              required
-            />
+    <Fragment>
+      <h1 className="mt-5 text-center">Login</h1>
+      <Form
+        className=""
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+      >
+        <Row className="mb-3">
+          <Form.Group
+            className="mx-auto"
+            as={Col}
+            md="8"
+            controlId="validationUsername"
+          >
+            <Form.Label>Username:</Form.Label>
+            <InputGroup hasValidation>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                aria-describedby="inputGroupPrepend"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid username.
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group
+            className="mx-auto"
+            as={Col}
+            md="8"
+            controlId="validationPassword"
+          >
+            <Form.Label>Password:</Form.Label>
+            <Form.Control type="text" placeholder="Password" required />
             <Form.Control.Feedback type="invalid">
-              Please choose a username.
+              Please provide a valid password.
             </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group> */}
-      </Row>
-      {/* <Row className="mb-3">
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>  */}
-    </Form>
-  )
+          </Form.Group>
+        </Row>
+        <div className="text-center">
+          <Button className="btn btn-success btn-block w-25" type="submit">Login</Button>
+          <p>
+            Don't have an account? <Link to="/register">register</Link>
+          </p>
+        </div>
+      </Form>
+    </Fragment>
+  );
 }
