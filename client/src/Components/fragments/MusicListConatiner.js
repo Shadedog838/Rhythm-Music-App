4d9fadc3c21737c57a03509f4525f1ec720e3547
sam8031 <sam8031@g.rit.edu>
2022-10-20 18:02:19 -0400

    Update MusicListConatiner.js

--- client/src/Components/fragments/MusicListConatiner.js
+++ client/src/Components/fragments/MusicListConatiner.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Fragment } from "react";
-
+import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   solid,
@@ -11,8 +11,11 @@ import {
 
 import "../assets/scss/MusicListContainer.scss";
 
+
 export default function MusicListConatiner() {
   const [songs, setSongs] = useState([]);
+  const [limit, setLimit] = useState(10);
+
   const getSongs = async () => {
     try {
       const response = await fetch("http://localhost:5000/song");
@@ -35,43 +38,56 @@ export default function MusicListConatiner() {
       : `${minutes}:${padTo2Digits(seconds)}`;
   };
 
+  const loadMore = () => {
+    setLimit(limit + 10);
+  };
+
   useEffect(() => {
     getSongs();
   }, []);
 
   console.log(songs);
   return (
-    <Fragment>
-      <table border="1" frame="void" rules="rows">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Artist</th>
-            <th>Album</th>
-            <th>Lenth</th>
-            <th>Listen #</th>
-            <th>Play</th>
-            <th>Add to Playlist</th>
-          </tr>
-        </thead>
-        <tbody>
-          {songs.map((song) => (
-            <tr key={songs.indexOf(song)}>
-              <td>{song.title}</td>
-              <td>{song.name}</td>
-              <td>{song.album_name}</td>
-              <td>{convertMsToMinuteSeconds(song.length)}</td>
-              <td>{song.times_played}</td>
-              <td>
-                <FontAwesomeIcon icon={solid("play-circle")} />
-              </td>
-              <td>
-                <FontAwesomeIcon icon={solid("plus-square")} />
-              </td>
+    <>
+      <Fragment>
+        <table border="1" frame="void" rules="rows">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Artist</th>
+              <th>Album</th>
+              <th>Lenth</th>
+              <th>Listen #</th>
+              <th>Play</th>
+              <th>Add to Playlist</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </Fragment>
+          </thead>
+          <tbody>
+            {songs
+              .filter((song) => songs.indexOf(song) < limit)
+              .map((song) => (
+                <tr key={songs.indexOf(song)}>
+                  <td>{song.title}</td>
+                  <td>{song.name}</td>
+                  <td>{song.album_name}</td>
+                  <td>{convertMsToMinuteSeconds(song.length)}</td>
+                  <td>{song.times_played}</td>
+                  <td>
+                    <FontAwesomeIcon icon={solid("play-circle")} />
+                  </td>
+                  <td>
+                    <FontAwesomeIcon icon={solid("plus-square")} />
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        <div className="text-center">
+          <Button className="btn btn-success btn-block w-25 mt-3 mb-3" onClick={loadMore}>
+            Load More
+          </Button>
+        </div>
+      </Fragment>
+    </>
   );
 }
