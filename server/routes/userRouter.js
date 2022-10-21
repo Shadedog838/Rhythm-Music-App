e6c039c40f59243a87f458271f3d873b5d8ea599
rk7572 <rk7527@rit.edu>
2022-10-21 17:37:43 -0400

    user login and register completed and tested

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,54 +1,71 @@
 // Routes for all USER related operations //
-const { query } = require("express");
+/*
+                    IMPLEMENTED METHODS 
+    * Register user
+    * login user
+*/
 const express = require("express");
 const router = express.Router();
 const pool = require("../db")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
-router.post("/register",async(req,res)=>{
-    var username = req.body.username
-    var fName= req.body.firstNname
-    var lName= req.body.lastname
-    var Password= req.body.password
-    var email= req.body.email
-    
-    var query = `SELECT  COUNT(SELECT username FROM users WHERE username=${username})`
-    var hasUser = await pool.query(query)
-    if(!hasUser){
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(Password, salt);
-        query = `INSERT INTO users (username, firstname, lastname, email, password, createddate, lastdateaccess) values (${username}, ${fName},${lName}, ${email},${hashedPassword},${Date.UTC()},${Date.UTC()}) `
-        try{
-            await pool.query(query)
-        }catch(e){
-            res.statusCode(500)
+
+
+//Register user
+router.post("/register", async (req, res) => {
+    var cusername = req.body.username
+    var fName = req.body.firstname
+    var lName = req.body.lastname
+    var Password = req.body.password
+    var email = req.body.email
+    query = `INSERT INTO users(username,firstname,lastname,email,password)VALUES($1,$2,$3,$4,$5)`
+    //checking if username or password exists
+    pool.query(`SELECT COUNT(*) FROM users WHERE username=$1 or email=$2 ;`, [cusername, email], async(error, value) => {
+        if (error) {
+            res.status(500)
         }
-    }else{
-        res.statusCode(409)
-    }
-})
-
-router.post("/login", async(req,res)=>{
-    var username = req.body.username
-    var password = req.body.password
-    var query = `SELECT password FROM users WHERE username = ${username}`
-    pool.query(query, async(error,result)=>{
-        if(error){
-            res.status(404)
+        console.log(value)
+        if (value.rowCount < 1 || Number(value.rows[0].count)>= 1) {
+            res.sendStatus(409)
         }else{
-            if(bcrypt.compare(result["password"], password)){
-                res.status(200).json(result)
+        const saltval = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(Password, saltval)
+        const queryInput = [cusername, fName, lName, email, hashedPassword]
+        //inserting new user with unique username and passowrd
+        pool.query(query, queryInput, (error, value) => {
+            if (error) {
+                console.log(error)
+                res.sendStatus(500)
             }
-            else{
-                res.status(404)
+            if (value) {
+                res.sendStatus(201)
             }
-        }
+        })}
     })
 
 })
 
+//Log in user 
+router.post("/login", async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    const query = `SELECT password FROM users WHERE username=$1`
 
+    //geting hashed password form the database
+    pool.query(query,[username], async(error,dataobj)=>{
+        if(error){
+            console.log(error)
+            res.sendStatus(500)
+        }
+        // comparig passwords 
+        if( dataobj.rowCount==1 && await bcrypt.compare(password,dataobj.rows[0].password)){
+            res.status(200).json({"message":"loggedin"})
+        }else{
+            res.status(401).json({"message":"loginFailed"})
+        }
+    })
+    
 
-
+})
 module.exports = router;
 
